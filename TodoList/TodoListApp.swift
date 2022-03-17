437b1ca6ec8b2d3f087522bfc6b244baaec3ff09
Bing Kuo <bingkuo@kkstream.com>
2022-03-17 16:16:58 +0800

    Part 1

--- TodoList/TodoListApp.swift
+++ TodoList/TodoListApp.swift
@@ -5,13 +5,26 @@
 //  Created by Bing Kuo on 2022/3/17.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct TodoListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(
+                store: Store(
+                    initialState: AppState(
+                        todos: [
+                            Todo(id: UUID(), description: "Milk", isComplete: false),
+                            Todo(id: UUID(), description: "Eggs", isComplete: false),
+                            Todo(id: UUID(), description: "Hand Soap", isComplete: false),
+                        ]
+                    ),
+                    reducer: appReducer,
+                    environment: AppEnvironment()
+                )
+            )
         }
     }
 }
